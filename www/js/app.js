80a34d79fe18714b4057e9b5fe27e0845b69eb55
Dan Lewis <jdanl@clemson.edu>
2013-10-09 17:28:45 -0400

    Make ios happy

--- www/js/app.js
+++ www/js/app.js
@@ -2,34 +2,34 @@
 define(['jquery', 'app/db', 'app/hello'],
 	function ($, db, hello) {
 
-		// Document is ready
-		$(function() {
-			var checkCordova, checkInterval, startup;
+		var checkCordova, startup;
 
-			// Start the app when the device is ready
-			startup = function() {
+		// Start the app when the device is ready
+		startup = function() {
+			// Wait for document ready
+			$(function() {
 				db.startup();
 				hello.startup();
 				hello.show();
-			};
+			});
+		};
 
-			// Check for Phonegap (Cordova) to avoid race conditions
-			// with the deviceready event
-			checkCordova = function() {
-				// Check if Cordova exists (multiple conditions  may be redundant)
-				if (window.cordova && window.cordova.exec && navigator.app) {
-					// Stop polling
-					clearInterval(checkInterval);
-					// Listen for the deviceready event
-					document.addEventListener('deviceready', startup, false);
-				}
-			};
+		// Check for Phonegap (Cordova) to avoid race conditions
+		// with the deviceready event
+		checkCordova = function checkCordova() {
+			// Check if Cordova exists
+			if (window.cordova && window.cordova.exec) {
+				// Listen for the deviceready event
+				document.addEventListener('deviceready', startup, false);
+			} else {
+				// If Cordova does not exist, check again in 16ms
+				setTimeout(checkCordova, 16);
+			}
+		};
 
-			// Poll for Phonegap.
-			// For local development, startup may need to be called directly
-			// since the deviceready event does not exist.
-			checkInterval = setInterval(checkCordova, 200);
-
-		});
+		// Poll for Phonegap.
+		// For local development, startup may need to be called directly
+		// since the deviceready event does not exist.
+		checkCordova();
 	}
 );
